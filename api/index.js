6c0860d8587ce01a074c1ae8c142047071a4bb41
Abhinay Reddy <abhinayreddy0112@gmail.com>
2025-05-06 11:44:17 +0700

    cors issue fix

--- api/index.js
+++ api/index.js
@@ -1,6 +1,25 @@
 // api/index.js
 const app = require('../app');
+const serverless = require('serverless-http');
 
-module.exports = (req, res) => {
-  app(req, res);
+const handler = serverless(app);
+
+// Custom CORS middleware for Vercel
+module.exports = async (req, res) => {
+  res.setHeader('Access-Control-Allow-Credentials', 'true');
+  res.setHeader('Access-Control-Allow-Origin', '*'); // Change to your frontend origin in production
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,OPTIONS,PATCH,DELETE,POST,PUT'
+  );
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'X-Requested-With, Accept, Content-Type, Authorization'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  return handler(req, res);
 };
